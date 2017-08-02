64e37d7fff9abf46e566a3361b861b9e4d3f32ac
unknown <marcelopspereira@gmail.com>
2017-08-01 21:44:54 -0300

    add cors

--- src/app.js
+++ src/app.js
@@ -30,12 +30,12 @@ app.use(bodyParser.urlencoded({
 }));
 
 // Habilita o CORS
-// app.use(function (req, res, next) {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-//     next();
-// });
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
 
 
 
