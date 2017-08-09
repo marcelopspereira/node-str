4b136f99e591e299c875264d1391335e7cd4a27f
unknown <marcelopspereira@gmail.com>
2017-08-09 20:23:11 -0300

    correção

--- src/config.js
+++ src/config.js
@@ -3,6 +3,5 @@ global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
 
 module.exports = {
     connectionString: 'mongodb://mps:mps@ds034807.mlab.com:34807/node-store',
-    sendgridKey: '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', //api_send_email
     containerConnectionString: 'SUA CONNECTION STRING'
 }
